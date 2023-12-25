34c2d63fe5ed86d9822477ec2e7a5dd1c42296b5
yuriy05 <dilayyura@gmail.com>
2023-12-26 01:24:41 +0200

    useReeducer in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
-import {useState} from 'react';
+import {useReducer} from 'react';
 import './App.css';
 
-function App() {
+type State = {
+  count: number
+}
 
-  const [increment, setIncrement] = useState<number>(0);
+type Action = {
+  type: ACTION_TYPE;
+}
+
+enum ACTION_TYPE  {
+  INCREMENT = "INCREMENT",
+  DECREMENT = "DECREMENT"
+}
+
+const stateReducer: React.Reducer<State, Action> = (
+  state: State,
+  action: Action
+): State => {
+  switch(action.type) {
+    case ACTION_TYPE.INCREMENT:
+      return {...state, count: state.count + 1};
+    case ACTION_TYPE.DECREMENT:
+      return {...state, count: state.count - 1};
+    default:
+      return state;
+  }
+};
+
+
+
+const App: React.FC<{}> = () => {
+  const [state, dispatch] = useReducer<React.Reducer<State, Action>,
+  State
+  >(
+    stateReducer,
+    { count: 0},
+    (state): State => ({
+      ...state,
+      count: 1,
+    })
+  );
+
+    const handleClick = () => {
+      dispatch({type: ACTION_TYPE.INCREMENT})
+    }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button>Set increment</button>
+        <p onClick={handleClick}>Count: {state.count}</p>
       </header>
     </div>
   );
